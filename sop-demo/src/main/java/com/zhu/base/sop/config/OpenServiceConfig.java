2ebc5544d35a7466f8c25c3f6e21a6c7b810f8b0
zhuhaibo <zhuhaibo@asinking.com>
2021-04-26 16:14:58 +0800

    新增：增加SOP配置注释

--- sop-demo/src/main/java/com/zhu/base/sop/config/OpenServiceConfig.java
+++ sop-demo/src/main/java/com/zhu/base/sop/config/OpenServiceConfig.java
@@ -4,6 +4,8 @@ import com.gitee.sop.servercommon.configuration.AlipayServiceConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * 所有接入Sop服务的项目，都新增该配置，用于SOP Gateway 感知接入的服务，拉取元数据
+ *
  * @author: ZhuHaiBo
  * @date: 2021/4/22  15:06
  */
