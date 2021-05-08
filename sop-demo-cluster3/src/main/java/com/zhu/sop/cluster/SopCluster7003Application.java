d7652fe4486cc3eda8bd333facabc1905a03192b
zhuhaibo <zhuhaibo@asinking.com>
2021-05-08 09:51:43 +0800

    新增：sop cluster 3

---
+++ sop-demo-cluster3/src/main/java/com/zhu/sop/cluster/SopCluster7003Application.java
@@ -0,0 +1,17 @@
+package com.zhu.sop.cluster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author: ZhuHaiBo
+ * @date: 2021/4/30  15:04
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SopCluster7003Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SopCluster7003Application.class, args);
+    }
+}
---
+++ sop-demo-cluster3/src/main/java/com/zhu/sop/cluster/controller/FirstController.java
@@ -0,0 +1,33 @@
+package com.zhu.sop.cluster.controller;
+
+import com.gitee.sop.servercommon.annotation.Open;
+import com.zhu.base.entity.BaseEntity;
+import com.zhu.base.util.ResultUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: ZhuHaiBo
+ * @date: 2021/4/30  15:01
+ */
+@Slf4j
+@RestController
+public class FirstController {
+
+    @Open(value = "sop.cluster", version = "2.0")
+    @GetMapping("/getCluster")
+    public ResultUtil<BaseEntity> getCluster2() {
+        log.info("请求到达Cluster3");
+        BaseEntity baseEntity = BaseEntity.builder().id(3003).username("Cluster3").build();
+        return ResultUtil.success(baseEntity);
+    }
+
+    //@Open(value = "sop.demo", version = "2.0")
+    //@GetMapping("/sopDemo")
+    //public ResultUtil<BaseEntity> getDemo() {
+    //    log.info("请求到达Cluster3");
+    //    BaseEntity baseEntity = BaseEntity.builder().id(3003).username("sopDemo").build();
+    //    return ResultUtil.success(baseEntity);
+    //}
+}
