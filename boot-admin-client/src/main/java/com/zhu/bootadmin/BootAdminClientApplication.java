fb9759f1586442b56a389196576f54a8f64146b8
zhuhaibo <zhuhaibo@asinking.com>
2021-05-18 18:15:35 +0800

    新增：springboot admin mail demo

--- boot-admin-client/src/main/java/com/zhu/bootadmin/BootAdminClientApplication.java
+++ boot-admin-client/src/main/java/com/zhu/bootadmin/BootAdminClientApplication.java
@@ -2,11 +2,13 @@ package com.zhu.bootadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author: ZhuHaiBo
  * @date: 2021/5/18  11:49
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class BootAdminClientApplication {
     public static void main(String[] args) {
